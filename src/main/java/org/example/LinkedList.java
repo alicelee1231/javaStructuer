c3d11d26f712aefd207d08580122add98164d3b3
alice <lsm@ziumks>
2023-03-28 17:56:52 +0900

    linked list 만드는 중

--- src/main/java/org/example/LinkedList.java
+++ src/main/java/org/example/LinkedList.java
@@ -1,23 +1,31 @@
 package org.example;
 
-import java.util.List;
+
 
 public class LinkedList {
 
-  //first와 last모두 linked_list의 처음 ele와 마지막 ele임
+  //first와 last모두 linked_list의 처음 ele와 마지막 ele임/ ele가 모여서 list가 되는거임
 
   int size;
   Element first;
   Element last;
-  List list; //util쓰면..안되겠지..?
 
-  //get이 있으니까
-  public String add(){
 
-    return null;
-  }
-
-  public int size(){
-    return size;
+  public void add(String x) {
+    Element el = new Element();
+    el.setValue(x);
+    el.setPrevElement(el.prev_element);
+    el.setNextElement(el.next_element);
+    if(first != null) {
+      //list는 서로 앞의 마지막 값을 알고 뒤의 처음 값을 안다...
+      //그럼 먼저 어떤 값이 있는지 가져오고..
+      //연결을 해야할듯..?
+      el.getValue();
+      el.getPrevElement();
+      el.getNextElement();
+    }else {
+      this.first = el;
+      this.last = el;
+    }
   }
 }
--- src/main/java/org/example/LinkedListMain.java
+++ src/main/java/org/example/LinkedListMain.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import java.util.LinkedList;
-
 public class LinkedListMain {
     public static void main(String[] args){
-        LinkedList bcd = new LinkedList();
-
+        LinkedListMain llm = new LinkedListMain();
+        llm.add("hello");
     }
 
 }
